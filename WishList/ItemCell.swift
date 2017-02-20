aec33056c5e382fad4b23828af8c9bed31bf52da
Nishant <nsood053@uottawa.ca>
2017-02-19 21:01:05 -0500

    - Added functionality to save data in persistence using core data

---
+++ WishList/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  WishList
+//
+//  Created by Nishant Sood on 2/19/17.
+//  Copyright © 2017 Nishant Sood. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+    func configureCell(item: Item){
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    
+    }
+}
